b3e943adb75d0774e7c60816a5a09127612db8a4
vitorfuziwara <vitor_fuziwara@outlook.com>
2026-05-21 13:14:08 -0300

    corrigi a classe pessoa

--- src/models/pessoa.py
+++ src/models/pessoa.py
@@ -1,17 +1,35 @@
-from pessoa import Pessoa
-
-class Paciente(Pessoa):
-    def __init__(self, nome:str, celular:int, cpf:str, idade:int):
-        super().__init__(nome, celular, cpf)
-        self.__idade = None
-        if isinstance(idade, int):
-            self.__idade = idade
+class Pessoa():
+    def __init__(self, nome:str, celular:int, cpf:str):
+        self.__nome = None
+        self.__celular = None
+        self.__cpf = None
+        if isinstance(nome, str):
+            self.__nome = nome
+        if isinstance(celular, int):
+            self.__celular = celular
+        if isinstance(cpf, str):
+            self.__cpf = cpf
     
     @property
-    def idade(self):
-        return self.__idade
-    @idade.setter
-    def idade(self, idade):
-        if isinstance(idade, int):
-            self.__idade = idade
-
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, nome):
+        if isinstance(nome, str):
+            self.__nome = nome
+    
+    @property
+    def celular(self):
+        return self.__celular
+    @celular.setter
+    def celular(self, celular):
+        if isinstance(celular, int):
+            self.__celular = celular
+    
+    @property
+    def cpf(self):
+        return self.__cpf
+    @cpf.setter
+    def cpf(self, cpf):
+        if isinstance(cpf, str):
+            self.__cpf = cpf
