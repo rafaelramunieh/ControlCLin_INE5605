75fba91a1a0efa2b2da4f81e1079586ee9a71290
rafaelramunieh <rafaelramunieh@hotmail.com>
2026-06-06 12:37:38 -0300

    feat: controlador e tela de sistema

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from controllers.controlador_sistema import ControladorSistema
+
+
+def main():
+    sistema = ControladorSistema()
+    sistema.abrir_menu()
+
+
+if __name__ == "__main__":
+    main()
