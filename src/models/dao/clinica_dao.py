742d06c0bd447ca6dbc4784c2887857558fdf4f7
vitorfuziwara <vitor_fuziwara@outlook.com>
2026-07-04 22:08:00 -0300

    implementei o codigo do DAO  de clinicas

---
+++ src/models/dao/clinica_dao.py
@@ -0,0 +1,23 @@
+from .abstract_dao import AbstractDAO
+
+class ClinicaDAO(AbstractDAO):
+    def __init__(self):
+        super().__init__("clinica.pkl")
+    
+    def add(self, nome, clinica):
+        from models.clinica import Clinica
+        if not isinstance(clinica, Clinica):
+            raise TypeError("O objeto deve ser uma instância da classe Clinica.")
+        super().add(nome, clinica)
+    
+    def get(self, nome):
+        from models.clinica import Clinica
+        clinica = super().get(nome)
+        return clinica
+    
+    def remove(self, nome):
+        from models.clinica import Clinica
+        clinica = super().get(nome)
+        if not isinstance(clinica, Clinica):
+            raise TypeError("O objeto deve ser uma instância da classe Clinica.")
+        super().remove(nome)
